bea01c6b8119c7c74149c19cda850bc02f610600
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-06-11 20:40:20 +0300

    refactoring example code

--- example_test.go
+++ example_test.go
@@ -19,6 +19,10 @@ import (
 // Proof that we know `a`, such that f(1, a, 2) = 8
 // a = 4
 
+func NewInt(val int64) *big.Int {
+	return mod(big.NewInt(val))
+}
+
 var inverse2 = new(big.Int).ModInverse(big.NewInt(2), bn256.Order)
 var inverse4 = new(big.Int).ModInverse(big.NewInt(4), bn256.Order)
 
@@ -59,36 +63,36 @@ func f2(xi []*bn256.G2, c []*big.Int, inverse *big.Int) *bn256.G2 {
 }
 
 func l1(xi []*bn256.G1) []*bn256.G1 {
-	la := f1(xi, []*big.Int{big.NewInt(6), mod(big.NewInt(-5)), big.NewInt(1)}, inverse2)
-	lw := f1(xi, []*big.Int{mod(big.NewInt(-4)), big.NewInt(5), mod(big.NewInt(-1))}, inverse2)
+	la := f1(xi, []*big.Int{NewInt(6), NewInt(-5), NewInt(1)}, inverse2)
+	lw := f1(xi, []*big.Int{NewInt(-4), NewInt(5), NewInt(-1)}, inverse2)
 	return []*bn256.G1{la, nil, lw, nil, nil}
 }
 
 func l2(xi []*bn256.G2) []*bn256.G2 {
-	la := f2(xi, []*big.Int{big.NewInt(6), mod(big.NewInt(-5)), big.NewInt(1)}, inverse2)
-	lw := f2(xi, []*big.Int{mod(big.NewInt(-4)), big.NewInt(5), mod(big.NewInt(-1))}, inverse2)
+	la := f2(xi, []*big.Int{NewInt(6), NewInt(-5), NewInt(1)}, inverse2)
+	lw := f2(xi, []*big.Int{NewInt(-4), NewInt(5), NewInt(-1)}, inverse2)
 	return []*bn256.G2{la, nil, lw, nil, nil}
 }
 
 func r2(xi []*bn256.G2) []*bn256.G2 {
-	ra := f2(xi, []*big.Int{big.NewInt(3), mod(big.NewInt(-4)), big.NewInt(1)}, nil)
-	rb := f2(xi, []*big.Int{big.NewInt(12), mod(big.NewInt(-13)), big.NewInt(3)}, inverse2)
-	rw := f2(xi, []*big.Int{big.NewInt(2), mod(big.NewInt(-3)), big.NewInt(1)}, inverse2)
-	rm := f2(xi, []*big.Int{mod(big.NewInt(-3)), big.NewInt(4), mod(big.NewInt(-1))}, nil)
+	ra := f2(xi, []*big.Int{NewInt(3), NewInt(-4), NewInt(1)}, nil)
+	rb := f2(xi, []*big.Int{NewInt(12), NewInt(-13), NewInt(3)}, inverse2)
+	rw := f2(xi, []*big.Int{NewInt(2), NewInt(-3), NewInt(1)}, inverse2)
+	rm := f2(xi, []*big.Int{NewInt(-3), NewInt(4), NewInt(-1)}, nil)
 	return []*bn256.G2{ra, rb, rw, rm, nil}
 }
 
 func o2(xi []*bn256.G2) []*bn256.G2 {
-	oa := f2(xi, []*big.Int{big.NewInt(3), mod(big.NewInt(-4)), big.NewInt(1)}, nil)
-	ob := f2(xi, []*big.Int{big.NewInt(3), mod(big.NewInt(-4)), big.NewInt(1)}, nil)
-	ow := f2(xi, []*big.Int{big.NewInt(2), mod(big.NewInt(-3)), big.NewInt(1)}, inverse2)
-	om := f2(xi, []*big.Int{big.NewInt(6), mod(big.NewInt(-5)), big.NewInt(1)}, inverse2)
-	ov := f2(xi, []*big.Int{mod(big.NewInt(-3)), big.NewInt(4), mod(big.NewInt(-1))}, nil)
+	oa := f2(xi, []*big.Int{NewInt(3), NewInt(-4), NewInt(1)}, nil)
+	ob := f2(xi, []*big.Int{NewInt(3), NewInt(-4), NewInt(1)}, nil)
+	ow := f2(xi, []*big.Int{NewInt(2), NewInt(-3), NewInt(1)}, inverse2)
+	om := f2(xi, []*big.Int{NewInt(6), NewInt(-5), NewInt(1)}, inverse2)
+	ov := f2(xi, []*big.Int{NewInt(-3), NewInt(4), NewInt(-1)}, nil)
 	return []*bn256.G2{oa, ob, ow, om, ov}
 }
 
 // a b w m v
-var inputGates = []*big.Int{big.NewInt(4), big.NewInt(2), big.NewInt(1), big.NewInt(8), big.NewInt(8)}
+var inputGates = []*big.Int{NewInt(4), NewInt(2), NewInt(1), NewInt(8), NewInt(8)}
 
 func big1(xi []*bn256.G1) *bn256.G1 {
 	var e = make([]*bn256.G1, 0, 5)
@@ -123,7 +127,7 @@ func big2(xi []*bn256.G2) *bn256.G2 {
 }
 
 func h(xi []*bn256.G2) *bn256.G2 {
-	c := []*big.Int{big.NewInt(6), mod(big.NewInt(-3)), big.NewInt(0)}
+	c := []*big.Int{NewInt(6), NewInt(-3), NewInt(0)}
 	var e = make([]*bn256.G2, 0, 3)
 	for i, val := range xi {
 		e = append(e, new(bn256.G2).ScalarMult(val, c[i]))
